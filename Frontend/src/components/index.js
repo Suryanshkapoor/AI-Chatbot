fb7ec67dbc98c293fa106cca36b71ab8efb7d2bd
Suryanshkapoor <suryanshkapoorji@gmail.com>
2024-01-26 14:22:35 +0530

    Update

--- Frontend/src/components/index.js
+++
@@ -1,5 +0,0 @@
-import RightSection from "./RightSection";
-import LeftSection from "./LeftSection";
-import QuestionAnswer from "./QuestionAnswer";
-
-export { RightSection, LeftSection, QuestionAnswer };
